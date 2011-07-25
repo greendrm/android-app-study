16f1dc15807196e850be3e039e951fd29cc434dd
greendrm <greendrm@gmail.com>
2011-07-25 18:13:08 +0900

    Android: Editor: update

--- Editor/src/greendrm/editor/EditorActivity.java
+++ Editor/src/greendrm/editor/EditorActivity.java
@@ -1,14 +1,19 @@
 package greendrm.editor;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class EditorActivity extends Activity {
+	private final static String TAG = "editor";
 	ListView list;
 	ArrayList<String> files;
 	ArrayAdapter<String> adapter;
@@ -27,5 +32,33 @@ public class EditorActivity extends Activity {
         
         adapter = new ArrayAdapter<String>(this, R.layout.listrow, files);
         list.setAdapter(adapter);
+    }
+    
+    public void createSDDir(String dirName) {
+    	String state = Environment.getExternalStorageState();
+		if (state.equals(Environment.MEDIA_MOUNTED)) {
+			File dir = Environment.getExternalStorageDirectory();
+			File file = new File(dir, dirName);
+			file.mkdir();
+		}
+		else {
+			//
+		}
+    }
+    
+    public void createSDFile(String fileName) {
+		String state = Environment.getExternalStorageState();
+		if (state.equals(Environment.MEDIA_MOUNTED)) {
+			File dir = Environment.getExternalStorageDirectory();
+			File file = new File(dir, fileName);
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				Log.e(TAG, "error creating file", e);
+			}
+		}
+		else {
+			//
+		}
     }
 }
